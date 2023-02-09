ccdd5eef60f543369f9e5458211f25a4501dc5ff
Jitain Sangwan <jitain.sangwan1997@gmail.com>
2023-02-10 00:43:51 +0530

    Write function Fix

--- MAX11137.c
+++ MAX11137.c
@@ -1,3 +1,16 @@
+/*
+ * MAX11137 Library
+ * Author: Jitain Sangwan (A.k.a Pheonix)
+ * 
+ * This library provides functions for interacting with the MAX11137 ADC IC.
+ * The functions enable the user to retrieve ADC counts from the MAX11137 and send
+ * them to a microcontroller for further processing.
+ * 
+ * The library is released under the MIT License.
+ * Feel free to use and modify the code for your projects.
+ */
+
+
 #include <stdio.h>
 #include "MAX11137.h"
 
@@ -8,4 +21,5 @@ bool MAX11137_WRITE(uint8_t CMD,uint16_t Data)
         Data |=CMD<11;
     }
     SPI_SEND_RECIEVE((uint8_t*)&Data);
+    return false;
 }
